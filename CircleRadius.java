47ca7b7570460b3e117c03026c98fafbcf96a3f6
shivanigaddala1019 <25wh5a0530@bvrithyderabad.edu.in>
2025-09-03 20:56:38 +0530

    Create CircleRadius.java

---
+++ CircleRadius.java
@@ -0,0 +1,10 @@
+package Com.sample;
+
+public class CircleRadius {
+	public static void main(String[]args) {
+	    float pi=3.14f;
+		int radius=2;
+		float area=pi*radius*radius;
+		System.out.println("Radius:"+radius+"\n"+"Area:"+area);
+		}
+}
