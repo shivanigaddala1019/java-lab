7bdae435fd5625b6c210211c97bc58daa1f06934
shivanigaddala1019 <25wh5a0530@bvrithyderabad.edu.in>
2025-09-03 20:58:39 +0530

    Create SumoFTwoNumbers.java

---
+++ SumoFTwoNumbers.java
@@ -0,0 +1,10 @@
+package Com.Sample;
+public class SumOfTwoNumbers {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 25;
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
+
